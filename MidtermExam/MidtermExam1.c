6c4b4eca6834cc1c8ec6e2b4eab622d154b7b471
Lee GyeongMuk <moogi3469@gmail.com>
2019-05-11 21:40:16 +0900

    중간고사 Addendum

--- MidtermExam/MidtermExam1.c
+++ MidtermExam/MidtermExam1.c
@@ -1,34 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /*
     Visual Studio Code에서 작성됨
     컴파일러: gcc (MinGW.org GCC-6.3.0-1) 6.3.0
 */
 
-int main(){
+int main() {
     int num1, num2;
     printf("\n***********************************");
     printf("\n 두자리 자연수의 곱 계산 프로그램 ");
     printf("\n***********************************");
 
     printf("\n 2자리 자연수를 입력하시오 : ");
-    scanf("%d",&num1);
+    scanf("%d", &num1);
 
     printf(" 2자리 자연수를 하나 더 입력하시오 : ");
-    scanf("%d",&num2);
+    scanf("%d", &num2);
 
-    printf("\n\t \t %4d",num1);
-    printf("\n\tx \t %4d",num2);
+    printf("\n\t \t %4d", num1);
+    printf("\n\tx \t %4d", num2);
     printf("\n***************************");
 
-    int num2_1=num2%10;          //num2의 1의 자리 숫자 저장
-    int num2_10=num2-num2_1;    //num2의 10의 자리 숫자 저장
-    
-    printf("\n(%d) x %4d \t is %4d",num1,num2_1,num1*num2_1);
-    printf("\n(%d) x %4d \t is %4d",num1,num2_10,num1*num2_10);
+    int num2_1 = num2 % 10;       //num2의 1의 자리 숫자 저장
+    int num2_10 = num2 - num2_1;  //num2의 10의 자리 숫자 저장
+
+    printf("\n(%d) x %4d \t is %4d", num1, num2_1, num1 * num2_1);
+    printf("\n(%d) x %4d \t is %4d", num1, num2_10, num1 * num2_10);
 
     printf("\n***************************");
-    printf("\n\t=\t %4d", num1 * num2 );
+    printf("\n\t=\t %4d", num1 * num2);
     printf("\n***************************");
     return 0;
 }
--- MidtermExam/MidtermExam4.cpp
+++ MidtermExam/MidtermExam4.cpp
@@ -1,70 +1,66 @@
-#include<iostream>
-#include<cmath>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-/* 2019년 1학기 고급프로그래밍 1 중간고사 (코딩) */ 
-/* 문제 4 좌표 연산 객체지향 프로그램 */ 
+/* 2019년 1학기 고급프로그래밍 1 중간고사 (코딩) */
+/* 문제 4 좌표 연산 객체지향 프로그램 */
 
 /*
-visual studio code에서 작성됨
-컴파일러: g++ (MinGW.org GCC-6.3.0-1) 6.3.0
+    visual studio code에서 작성됨
+    컴파일러: g++ (MinGW.org GCC-6.3.0-1) 6.3.0
 */
 
+class CCord {
+   public:
+    CCord(int nParam1, int nParam2) {
+        xVal = nParam1;
+        yVal = nParam2;
+    }
+    CCord& operator=(CCord& rhs) {
+        this->xVal = rhs.xVal;
+        this->yVal = rhs.yVal;
 
-class CCord
-{
-	public:
-		CCord(int nParam1, int nParam2) 
-		{
-			xVal=nParam1;
-            yVal=nParam2;
-		}
-		CCord& operator=(CCord &rhs)
-		{
-			this->xVal=rhs.xVal;
-            this->yVal=rhs.yVal;
+        return *this;
+    }
+    CCord operator+(CCord rhs) {
+        CCord result(0, 0);
+        result.xVal = this->xVal + rhs.xVal;
+        result.yVal = this->xVal + rhs.yVal;
 
-            return *this;
-		}
-		CCord operator+(CCord rhs)
-		{
-			CCord result;
-            
-		}
-		void print(string nParam)
-		{
-			cout << nParam <<" 좌표 (" << xVal << "," << yVal << ")" << endl;
-		}
-		void distance(string nParam)
-		{
-			cout << nParam << " 거리 :  " << sqrt(xVal*xVal + yVal*yVal) << endl;
-		}
+        return result;
+    }
+    void print(string nParam) {
+        cout << nParam << " 좌표 (" << xVal << "," << yVal << ")" << endl;
+    }
+    void distance(string nParam) {
+        cout << nParam << " 거리 :  " << sqrt(xVal * xVal + yVal * yVal) << endl;
+    }
 
-	private:	
-		double xVal;
-		double yVal;
+   private:
+    double xVal;
+    double yVal;
 };
 
-int main()
-{
+int main() {
+    cout << "*********************************************" << endl;
+    cout << "  문제 4.  좌표 연산 객체지향 프로그램" << endl;
+    cout << "*********************************************" << endl;
+    cout << endl;
 
-	cout << "*********************************************" << endl;
-	cout << "  문제 4.  좌표 연산 객체지향 프로그램" << endl; 
-	cout << "*********************************************" << endl;
-	cout << endl;
+    CCord t1(1, 1);
+    CCord t2(2, 3);
+    t1.print("t1");
+    t2.print("t2");
 
-	CCord t1 (1,1);
-	CCord t2 (2,3);
-	t1.print("t1");
-	t2.print("t2");
+    CCord t3 = t1 + t2;
 
-	CCord t3 = t1 + t2;
-	
-	cout << endl << "t3 = t1 + t2 " << endl << endl;
-	
-	t3.print("t3");
-	t3.distance("t3");
-	
-	return 0;
+    cout << endl
+         << "t3 = t1 + t2 " << endl
+         << endl;
+
+    t3.print("t3");
+    t3.distance("t3");
+
+    return 0;
 }
